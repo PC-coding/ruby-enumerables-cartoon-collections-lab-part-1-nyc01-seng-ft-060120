3f4718f28c3fe2550006ec35e1310a5741d5afcf
Partap Chauhan <partapchauhan8@gmail.com>
2020-05-24 19:27:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  array = ["Hoggle", "Ludo", "Sir Didymus"]
+  array = ["Dopey", "Ludo", "Sir Didymus"]
   array.each do |array|
     puts "Hello #{array.capitalize}!"
   end
