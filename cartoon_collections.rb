99186225f5334251b730ee0778846c2291a9311a
Partap Chauhan <partapchauhan8@gmail.com>
2020-05-24 19:33:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def greet_characters(array)
   # Print a custom greeting for each element
   characters_array = ["Dopey", "Grumpy", "Bashful", "Gimli", "Thorin", "Balin", "Gloin"]
   characters_array.each do |characters_array|
-    puts "Hello #[characters_array]!"
+    puts "Hello #{characters_array}!"
   end
 end
 
