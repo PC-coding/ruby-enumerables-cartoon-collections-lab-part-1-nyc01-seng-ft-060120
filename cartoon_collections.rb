49b68c94f82b1f64a9dec93b3b830d1df1dd7f0c
Partap Chauhan <partapchauhan8@gmail.com>
2020-05-24 19:37:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,9 +2,9 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  characters_array = ["Dopey", "Grumpy", "Bashful", "Gimli", "Thorin", "Balin", "Gloin"]
-  characters_array.each do |characters_array|
-    puts greet_characters(characters_array)
+  array = ["Dopey", "Grumpy", "Bashful", "Gimli", "Thorin", "Balin", "Gloin"]
+  array.each do |characters|
+    puts 
   end
 end
 
